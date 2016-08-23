0b5adf776a07baa082dc00a022a1ce761b8920db
yurizhuravel <zhuravel@gmail.com>
2016-08-23 15:58:26 +0100

    Adds individual restaurant pages

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,23 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -6,7 +6,7 @@ feature 'Restaurants' do
     scenario 'should ask to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants added yet'
-      expect(page).to have_button 'Add a restaurant'
+      expect(page).to have_link 'Add a restaurant'
     end
   end
 
@@ -21,4 +21,44 @@ feature 'Restaurants' do
       expect(page).not_to have_content 'No restaurants added yet'
     end
   end
+
+  context 'creating restaurants' do
+    scenario 'shows user a form to fill, then creates a restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'Etsu'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'Etsu'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'viewing restaurants' do
+
+    let!(:etsu){ Restaurant.create(name:'Etsu') }
+
+    scenario 'lets users view individual restaurants' do
+      visit '/restaurants'
+      click_link 'Etsu'
+      expect(page).to have_content 'Etsu'
+    end
+  end
+
+  context 'editing restaurants' do
+
+  before { Restaurant.create name: 'Etsu', description: 'Cheap fresh sushi' }
+
+  scenario 'let a user edit a restaurant' do
+   visit '/restaurants'
+   click_link 'Edit Etsu'
+   fill_in 'Name', with: 'Etsu Sushi'
+   fill_in 'Description', with: 'Cheap fresh sushi'
+   click_button 'Update Restaurant'
+   expect(page).to have_content 'Etsu Sushi'
+   expect(page).to have_content 'Cheap fresh sushi'
+   expect(current_path).to eq '/restaurants'
+  end
+
+end
+
 end
